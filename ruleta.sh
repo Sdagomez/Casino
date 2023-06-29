a82acf3573ebbe3fa5a6b9a8f66d4c8af204aa0e
Sdagomez <sgomez7048@gmail.com>
2023-06-29 18:34:22 -0500

    fix martingala function

--- ruleta.sh
+++ ruleta.sh
@@ -13,7 +13,7 @@ grayColour="\e[0;37m\033[1m"
 
 
 function ctrl_c(){
-  echo -e "\n\n[!] Saliendo....\n"
+  echo -e "\n\n${yellowColour}[!]${redColour} Saliendo.... ${endColour}\n"
   tput cnorm;  exit 1
 }
 
@@ -36,41 +36,51 @@ function martingala (){
   echo -e "\n${yellowColour}[+]${blueColour} Vamos a jugar con la cantidad inicial de $yellowColour $initial_bed${endColour}${blueColour} a $yellowColour$par_impar ${endColour}"
 
   backup_bed=$initial_bed
+  play_counter=1
+  jugadas_malas="[ "
 
   tput civis
   while true; do
     money=$(($money-$initial_bed))
-    echo -e "\n${yellowColour}[+]${blueColour} Acabas de apostar ${yellowColour} $initial_bed € ${endColour}${blueColour} y tienes ${endColour}${yellowColour} $money € ${endColour}"
+#    echo -e "\n${yellowColour}[+]${blueColour} Acabas de apostar ${yellowColour} $initial_bed € ${endColour}${blueColour} y tienes ${endColour}${yellowColour} $money € ${endColour}"
     random_number="$(($RANDOM % 37))"
-    echo -e "${yellowColour}[+]${blueColour} El nùmero que ha salido es: ${endColour}${yellowColour}$random_number${endColour}"
+#    echo -e "${yellowColour}[+]${blueColour} El nùmero que ha salido es: ${endColour}${yellowColour}$random_number${endColour}"
 
     if [[ ! "$money" -le 0 ]]; then
       if [[ "$par_impar" == "par" ]]; then 
         if [[ "$(($random_number % 2))" -eq 0 ]]; then
           if [[ "$random_number" -eq 0 ]]; then
-            echo -e "${yellowColour}[!]${redColour} Ha salido 0, por lo tanto perdiste${endColour}"
+#            echo -e "${yellowColour}[!]${redColour} Ha salido 0, por lo tanto perdiste${endColour}"
             initial_bed=$(($initial_bed*2))
-            echo -e "${yellowColour}[+]${redColour} Ahora mismo te quedas en ${endColour}${yellowColour} $money €  ${endColour}"
+            jugadas_malas+="$random_number "
+#            echo -e "${yellowColour}[+]${redColour} Ahora mismo te quedas en ${endColour}${yellowColour} $money €  ${endColour}"
           else
-            echo -e "${yellowColour}[+]${blueColour} EL nùmero es par, Ganaste!!! ${endColour}"
+#            echo -e "${yellowColour}[+]${blueColour} EL nùmero es par, Ganaste!!! ${endColour}"
             reward=$(($initial_bed*2))
-            echo -e "${yellowColour}[+]${purpleColour} Ganas un total de: ${endColour}${yellowColour} $reward € ${endColour}"
+#            echo -e "${yellowColour}[+]${purpleColour} Ganas un total de: ${endColour}${yellowColour} $reward € ${endColour}"
             money=$(($money+$reward))
-            echo -e "${yellowColour}[+]${blueColour} Tienes un total de: ${endColour}${yellowColour} $money € ${endColour}"
+#            echo -e "${yellowColour}[+]${blueColour} Tienes un total de: ${endColour}${yellowColour} $money € ${endColour}"
             initial_bed=$backup_bed
+
+            jugadas_malas=""
           fi
         else
-          echo -e "${yellowColour}[+]${redColour} El nùmero es impar, Pierdes!!! ${endColour}"
+  #        echo -e "${yellowColour}[+]${redColour} El nùmero es impar, Pierdes!!! ${endColour}"
           initial_bed=$(($initial_bed*2))
-          echo -e "${yellowColour}[+]${redColour} Ahora mismo te quedas en ${endColour}${yellowColour} $money €  ${endColour}"
-        fi
-        sleep 2     
+          jugadas_malas+="$random_number "
+#          echo -e "${yellowColour}[+]${redColour} Ahora mismo te quedas en ${endColour}${yellowColour} $money €  ${endColour}"
+        fi     
       fi
     else
+      echo -e "${yellowColour}[!]${redColour} Te has quedado sin dinero${endColour}\n"
+      echo -e "${yellowColour}[+]${grayColour} Han habido un total de: ${endColour}${yellowColour} $play_counter ${endColour}${grayColour} jugadas ${endColour}\n"
+      echo -e "${yellowColour}[+]${grayColour} A continuaciòn se van a representar las malas jugadas consecutivas que han salido: ${endColour}\n"
+      echo -e "${bluewColour} $jugadas_malas ${endColour}\n"
 
-      echo -e "${yellowColour}[!]${redColour} Te has quedado sin dinero${endColour}"
       tput cnorm;exit 0
     fi
+
+    let play_counter+=1
   done
 
   tput cnorm
@@ -86,7 +96,6 @@ while getopts "m:t:h" arg; do
 done
 
 if [[ $money ]] && [[ $technique ]]; then
-  echo "golang"
   if [[ "$technique" == "martingala" ]]; then
     martingala
   else
